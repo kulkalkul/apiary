2c962108fc6d0f94e4efb755a3ea907d7b506679
Bora <yokunjon@gmail.com>
2023-02-27 22:51:58 +0300

    Export HiveError from hive_client

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -2,4 +2,4 @@ mod hive_client;
 mod method;
 
 pub use method::{Method, EmptyParams};
-pub use hive_client::{HiveNode, HiveNodes, HiveClient};
+pub use hive_client::{HiveNode, HiveNodes, HiveClient, HiveError};
